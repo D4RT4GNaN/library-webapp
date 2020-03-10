fadec51f8b4581574a81c3be778253b5790de416
D4RT4GNaN <maxime.blaise06@gmail.com>
2020-03-10 15:09:10 +0100

    Creation of the webapp

---
+++ src/test/java/org/openclassroom/project/librarywebapp/LibraryWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package org.openclassroom.project.librarywebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
