9a6b52a0d5faa6253e3e581dad922968137559b6
D4RT4GNaN <maxime.blaise06@gmail.com>
2020-03-10 19:29:46 +0100

    Added index.html with search bar

--- src/main/java/org/openclassroom/project/librarywebapp/controllers/IndexController.java
+++ src/main/java/org/openclassroom/project/librarywebapp/controllers/IndexController.java
@@ -1,10 +1,14 @@
 package org.openclassroom.project.librarywebapp.controllers;
 
+import generated.libraryservice.Book;
 import generated.libraryservice.LibraryService;
 import generated.libraryservice.LibraryServiceService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.*;
 
 @Controller
 public class IndexController {
@@ -14,8 +18,31 @@ public class IndexController {
 
     @GetMapping("/")
     public String showAll(Model model) {
-        model.addAttribute("books", libraryService.getBooksWithKeyword(""));
+        List<Book> books = libraryService.getBooksWithKeyword("");
+
+        model.addAttribute("books", books);
+        model.addAttribute("categories", getCategories(books));
+
         return "index";
+    }
+
+    @GetMapping("/search")
+    public String search(Model model, @RequestParam("searchString") String searchString) {
+        List<Book> books = libraryService.getBooksWithKeyword(searchString);
+
+        model.addAttribute("books", books);
+        model.addAttribute("categories", getCategories(books));
+
+        return "index";
+    }
+
+    private List<String> getCategories(List<Book> books) {
+        Set<String> categories = new HashSet<>();
+        for (Book book : books) {
+            categories.add(book.getCategory());
+        }
+
+        return new ArrayList<>(categories);
     }
 
 }
